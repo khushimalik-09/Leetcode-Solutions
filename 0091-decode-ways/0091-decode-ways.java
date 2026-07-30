c2c1c734c5376e96c5a3c77337085b6cd50666ba
Khushi Malik <khushi.malikk456@gmail.com>
2026-07-30 12:32:45 +0530

    Time: 1 ms (79.52%), Space: 43.7 MB (5.55%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int dp[]=new int [n+1];
+        dp[0]=1;
+        dp[1]=s.charAt(0)=='0'?0:1;
+        for(int i=2;i<=n;i++){
+            int singledigit =  Integer.valueOf(s.substring(i-1,i));
+            int doubledigit = Integer.valueOf(s.substring(i-2,i));
+            if(singledigit >= 1) dp[i]+=dp[i-1];
+            if(doubledigit>=10 && doubledigit<=26) dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+}
