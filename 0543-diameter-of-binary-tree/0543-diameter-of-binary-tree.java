fe57ce7b7c32447cfbdb00bb0c799eacb52271c9
Khushi Malik <khushi.malikk456@gmail.com>
2026-08-11 19:07:31 +0530

    Time: 0 ms (100%), Space: 47 MB (74.38%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,30 +14,17 @@
  * }
  */
 class Solution {
+    private int max=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null)return 0;
-        Map<TreeNode, Integer> map = new HashMap<>();
-        Stack<TreeNode> st = new Stack<>();
-        int diameter =0;
-        st.push(root);
+        computediameter(root);
+        return max;
+    }
+    private int computediameter(TreeNode node){
+        if(node==null)return 0;
         
-        while(!st.isEmpty()){
-            TreeNode node = st.peek();
-            if(node.left!=null && !map.containsKey(node.left)){
-                st.push(node.left);
-            }
-            else if(node.right!=null && !map.containsKey(node.right)){
-                st.push(node.right);
-            }
-            else{
-                st.pop();
-                int leftDepth = map.getOrDefault(node.left,0);
-                int rightDepth = map.getOrDefault(node.right,0);
-                
-                map.put(node,1+Math.max(leftDepth,rightDepth));
-                diameter = Math.max(diameter, leftDepth+rightDepth);
-            }
-        }
-        return diameter;
+        int left = computediameter(node.left);
+        int right = computediameter(node.right);
+        max = Math.max(max,left+right);
+        return 1+Math.max(left,right);
     }
 }
