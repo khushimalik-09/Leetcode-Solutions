899d7cea8ad09c9b33b2a5c9d1fec71ee029323b
Khushi Malik <khushi.malikk456@gmail.com>
2026-08-26 19:46:40 +0530

    Time: 1204 ms (34.34%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,11 @@
+SELECT s.student_id,s.student_name,su.subject_name,COUNT(e.subject_name) as attended_exams
+FROM Students as s
+CROSS JOIN 
+Subjects as su
+LEFT JOIN 
+Examinations as e
+ON s.student_id = e.student_id
+AND su.subject_name = e.subject_name
+
+GROUP BY s.student_id,su.subject_name,s.student_name
+ORDER BY s.student_id,su.subject_name
