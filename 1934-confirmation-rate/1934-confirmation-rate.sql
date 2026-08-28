d9168cb516194eb41b8a4cf43b3a0b7b792e7765
Khushi Malik <khushi.malikk456@gmail.com>
2026-08-28 19:39:35 +0530

    Time: 595 ms (98.53%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,6 @@
+select s.user_id,
+round(sum(case when c.action="confirmed" then 1 else 0 end)/count(*),2) as confirmation_rate
+from Signups as s
+left join Confirmations as c
+on s.user_id = c.user_id
+group by s.user_id
